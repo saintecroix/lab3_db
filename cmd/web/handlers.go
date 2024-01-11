2d4b4b1430493ce578d1838842baf4eef5ffcc37
Kirill Markov <saintecroix@yandex.ru>
2024-01-11 19:30:24 +0300

    11.01.2024

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 type Gruz struct {
@@ -225,6 +226,7 @@ func (app *application) input(w http.ResponseWriter, r *http.Request) {
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		app.serverError(w, err) // Использование помощника serverError()
+		return
 	}
 
 	type Jopa struct {
@@ -241,44 +243,32 @@ func (app *application) input(w http.ResponseWriter, r *http.Request) {
 		State: getState(db), Station: getStation(db), Wagon: getWagon(db)})
 	if err != nil {
 		app.serverError(w, err) // Использование помощника serverError()
+		return
 	}
 }
 
 /*----------------------------------------------------------------------------------------*/
 
 func (app *application) save_application(w http.ResponseWriter, r *http.Request) {
-	Number := r.FormValue("Number")
-	Reg_date := r.FormValue("Reg_date")
-	Status := r.FormValue("Status")
-	Provide_date := r.FormValue("Provide_date")
-	Departure_type := r.FormValue("Departure_type")
-	Goods := r.FormValue("Goods")
-	Origin_state := r.FormValue("Origin_state")
-	Enter_station := r.FormValue("Enter_station")
-	Region_depart := r.FormValue("Region_depart")
-	Road_depart := r.FormValue("Road_depart")
-	Station_depart := r.FormValue("Station_depart")
-	Consigner := r.FormValue("Consigner")
-	State_destination := r.FormValue("State_destination")
-	Exit_station := r.FormValue("Exit_station")
-	Region_destination := r.FormValue("Region_destination")
-	Road_destination := r.FormValue("Road_destination")
-	Station_destination := r.FormValue("Station_destination")
-	Consignee := r.FormValue("Consignee")
-	Wagon_type := r.FormValue("Wagon_type")
-	Property := r.FormValue("Property")
-	Wagon_owner := r.FormValue("Wagon_owner")
-	Payer := r.FormValue("Payer")
-	Road_owner := r.FormValue("Road_owner")
-	Transport_manager := r.FormValue("Transport_manager")
-	Tons_declared := r.FormValue("Tons_declared")
-	Wagon_declared := r.FormValue("Wagon_declared")
-	Wagon_accepted := r.FormValue("Wagon_accepted")
-	Filing_date := r.FormValue("Filing_date")
-	Agreement_date := r.FormValue("Agreement_date")
-	Approval_date := r.FormValue("Approval_date")
-	Start_date := r.FormValue("Start_date")
-	Stop_date := r.FormValue("Stop_date")
+	number, _ := strconv.Atoi(r.FormValue("Number"))
+	reg_date := r.FormValue("Reg_date")
+	status := r.FormValue("Status")
+	provide_date := r.FormValue("Provide_date")
+	departure_type := r.FormValue("Departure_type")
+	property := r.FormValue("Property")
+	wagon_owner := r.FormValue("Wagon_owner")
+	payer := r.FormValue("Payer")
+	road_owner := r.FormValue("Road_owner")
+	transport_manager := r.FormValue("Transport_manager")
+	tons_declared, _ := strconv.Atoi(r.FormValue("Tons_declared"))
+	tons_accepted, _ := strconv.Atoi(r.FormValue("Tons_accepted"))
+	wagon_declared, _ := strconv.Atoi(r.FormValue("Wagon_declared"))
+	wagon_accepted, _ := strconv.Atoi(r.FormValue("Wagon_accepted"))
+	filing_date := r.FormValue("Filing_date")
+	agreement_date := r.FormValue("Agreement_date")
+	approval_date := r.FormValue("Approval_date")
+	start_date := r.FormValue("Start_date")
+	stop_date := r.FormValue("Stop_date")
 
 	db, errsql := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/lab3")
 	if errsql != nil {
@@ -287,23 +277,356 @@ func (app *application) save_application(w http.ResponseWriter, r *http.Request)
 
 	defer db.Close()
 
-	insert, err := db.Query(fmt.Sprintf("INSERT INTO 'application' "+
-		"('Number', 'Reg_date', 'Status', 'Provide_date', 'Departure_type', 'Goods', 'Origin_state', "+
-		"'Enter_station', 'Region_depart', 'Road_depart', 'Station_depart', 'Consigner', 'State_destination', "+
-		"'Exit_station', 'Region_destination', 'Road_destination', 'Station_destination', 'Consignee', "+
-		"'Wagon_type', 'Property', 'Wagon_owner', 'Payer', 'Road_owner', 'Transport_manager', 'Tons_declared', "+
-		"'Tons_accepted', 'Wagon_declared', 'Wagon_accepted', 'Filing_date', 'Agreement_date', 'Approval_date', "+
-		"'Start_date', 'Stop_date') VALUES('%d', '%s', '%s', '%s', '%s', '%d', '%d', '%d', '%d', '%d', '%d', "+
+	goods, _ := strconv.Atoi(r.FormValue("Goods"))
+
+	origin_state, _ := strconv.Atoi(r.FormValue("Origin_state"))
+
+	enter_station, _ := strconv.Atoi(r.FormValue("Enter_station"))
+
+	region_depart, _ := strconv.Atoi(r.FormValue("Region_depart"))
+
+	road_depart, _ := strconv.Atoi(r.FormValue("Road_depart"))
+
+	station_depart, _ := strconv.Atoi(r.FormValue("Station_depart"))
+
+	consigner, _ := strconv.Atoi(r.FormValue("Consigner"))
+
+	state_destination, _ := strconv.Atoi(r.FormValue("State_destination"))
+
+	exit_station, _ := strconv.Atoi(r.FormValue("Exit_station"))
+
+	region_destination, _ := strconv.Atoi(r.FormValue("Region_destination"))
+
+	road_destination, _ := strconv.Atoi(r.FormValue("Road_destination"))
+
+	station_destination, _ := strconv.Atoi(r.FormValue("Station_destination"))
+
+	consignees, _ := strconv.Atoi(r.FormValue("Consignee"))
+
+	wagon_type, _ := strconv.Atoi(r.FormValue("Wagon_type"))
+
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO `application` "+
+		"(`Number`, `Reg_date`, `Status`, `Provide_date`, `Departure_type`, `Goods`, `Origin_state`, "+
+		"`Enter_station`, `Region_depart`, `Road_depart`, `Station_depart`, `Consigner`, `State_destination`, "+
+		"`Exit_station`, `Region_destination`, `Road_destination`, `Station_destination`, `Consignee`, "+
+		"`Wagon_type`, `Property`, `Wagon_owner`, `Payer`, `Road_owner`, `Transport_manager`, `Tons_declared`, "+
+		"`Tons_accepted`, `Wagon_declared`, `Wagon_accepted`, `Filing_date`, `Agreement_date`, `Approval_date`, "+
+		"`Start_date`, `Stop_date`) VALUES('%d', '%s', '%s', '%s', '%s', '%d', '%d', '%d', '%d', '%d', '%d', "+
 		"'%d', '%d', '%d', '%d', '%d', '%d', '%d', '%d', '%s', '%s', '%s', '%s', '%s', '%d', '%d', '%d', '%d', "+
-		"'%s', '%s', '%s', '%s', '%s')"), Number, Reg_date, Status, Provide_date, Departure_type, Goods, Origin_state,
-		Enter_station, Region_depart, Road_depart, Station_depart, Consigner, State_destination, Exit_station,
-		Region_destination, Road_destination, Station_destination, Consignee, Wagon_type, Property, Wagon_owner,
-		Payer, Road_owner, Transport_manager, Tons_declared, Wagon_declared, Wagon_accepted, Filing_date, Agreement_date,
-		Approval_date, Start_date, Stop_date)
+		"'%s', '%s', '%s', '%s', '%s')", number, reg_date, status, provide_date, departure_type, goods, origin_state,
+		enter_station, region_depart, road_depart, station_depart, consigner, state_destination, exit_station,
+		region_destination, road_destination, station_destination, consignees, wagon_type, property, wagon_owner,
+		payer, road_owner, transport_manager, tons_declared, tons_accepted, wagon_declared, wagon_accepted, filing_date,
+		agreement_date, approval_date, start_date, stop_date))
 	if err != nil {
-		panic(err)
+		app.serverError(w, err)
+		return
 	}
 	defer insert.Close()
 
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+/*----------------------------------------------------------------------------------------*/
+
+func (app *application) addGruz(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"./ui/html/add_gruz.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	db, errsql := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/lab3")
+	if errsql != nil {
+		panic(errsql)
+	}
+
+	defer db.Close()
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err) // Использование помощника serverError()
+		return
+	}
+
+	type Jopa struct {
+		Gruz []Gruz
+	}
+
+	err = ts.Execute(w, Jopa{Gruz: getGruz(db)})
+	if err != nil {
+		app.serverError(w, err) // Использование помощника serverError()
+		return
+	}
+}
+
+/*----------------------------------------------------------------------------------------*/
+
+func (app *application) save_gruz(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("Name")
+	etsng := r.FormValue("ETSNG")
+	gng := r.FormValue("GNG")
+
+	db, errsql := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/lab3")
+	if errsql != nil {
+		panic(errsql)
+	}
+
+	defer db.Close()
+
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO `gruz` "+
+		"(`Name`, `ETSNG`, `GNG`) VALUES('%s', '%s', '%s')", name, etsng, gng))
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	defer insert.Close()
+
+	http.Redirect(w, r, "/input", http.StatusSeeOther)
+}
+
+/*----------------------------------------------------------------------------------------*/
+
+func (app *application) add_state(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"./ui/html/add_state.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	db, errsql := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/lab3")
+	if errsql != nil {
+		panic(errsql)
+	}
+
+	defer db.Close()
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err) // Использование помощника serverError()
+		return
+	}
+
+	type Jopa struct {
+		State []State
+	}
+
+	err = ts.Execute(w, Jopa{State: getState(db)})
+	if err != nil {
+		app.serverError(w, err) // Использование помощника serverError()
+		return
+	}
+}
+
+/*----------------------------------------------------------------------------------------*/
+
+func (app *application) save_state(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("Name")
+
+	db, errsql := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/lab3")
+	if errsql != nil {
+		panic(errsql)
+	}
+
+	defer db.Close()
+
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO `state` "+
+		"(`Name`) VALUES('%s')", name))
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	defer insert.Close()
+
+	http.Redirect(w, r, "/input", http.StatusSeeOther)
+}
+
+/*----------------------------------------------------------------------------------------*/
+
+func (app *application) add_station(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"./ui/html/add_station.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	db, errsql := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/lab3")
+	if errsql != nil {
+		panic(errsql)
+	}
+
+	defer db.Close()
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err) // Использование помощника serverError()
+		return
+	}
+
+	type Jopa struct {
+		Station []Station
+	}
+
+	err = ts.Execute(w, Jopa{Station: getStation(db)})
+	if err != nil {
+		app.serverError(w, err) // Использование помощника serverError()
+		return
+	}
+}
+
+/*----------------------------------------------------------------------------------------*/
+
+func (app *application) save_station(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("Name")
+	esr := r.FormValue("ESR")
+	road := r.FormValue("Road")
+
+	db, errsql := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/lab3")
+	if errsql != nil {
+		panic(errsql)
+	}
+
+	defer db.Close()
+
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO `station` "+
+		"(`Name`,`ESR`, `Road`) VALUES('%s', `%s`, `%s`)", name, esr, road))
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	defer insert.Close()
+
+	http.Redirect(w, r, "/input", http.StatusSeeOther)
+}
+
+/*----------------------------------------------------------------------------------------*/
+
+func (app *application) add_region(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"./ui/html/add_region.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	db, errsql := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/lab3")
+	if errsql != nil {
+		panic(errsql)
+	}
+
+	defer db.Close()
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err) // Использование помощника serverError()
+		return
+	}
+
+	type Jopa struct {
+		Region []Region
+	}
+
+	err = ts.Execute(w, Jopa{Region: getRegion(db)})
+	if err != nil {
+		app.serverError(w, err) // Использование помощника serverError()
+		return
+	}
+}
+
+/*----------------------------------------------------------------------------------------*/
+
+func (app *application) save_region(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("Name")
+
+	db, errsql := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/lab3")
+	if errsql != nil {
+		panic(errsql)
+	}
+
+	defer db.Close()
+
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO `region` "+
+		"(`Name`) VALUES('%s')", name))
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	defer insert.Close()
+
+	http.Redirect(w, r, "/input", http.StatusSeeOther)
+}
+
+/*----------------------------------------------------------------------------------------*/
+
+func (app *application) add_road(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"./ui/html/add_road.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	db, errsql := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/lab3")
+	if errsql != nil {
+		panic(errsql)
+	}
+
+	defer db.Close()
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err) // Использование помощника serverError()
+		return
+	}
+
+	type Jopa struct {
+		Road []Road
+	}
+
+	err = ts.Execute(w, Jopa{Road: getRoad(db)})
+	if err != nil {
+		app.serverError(w, err) // Использование помощника serverError()
+		return
+	}
+}
+
+/*----------------------------------------------------------------------------------------*/
+
+func (app *application) save_road(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("Name")
+	length, _ := strconv.Atoi(r.FormValue("Length"))
+
+	db, errsql := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/lab3")
+	if errsql != nil {
+		panic(errsql)
+	}
+
+	defer db.Close()
+
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO `road` "+
+		"(`Name`, `Length`) VALUES('%s', '%d')", name, length))
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	defer insert.Close()
+
+	http.Redirect(w, r, "/input", http.StatusSeeOther)
+}
+
+//func getid(db *sql.DB, col string, name string) int {
+//	raw, err := db.Query(fmt.Sprintf("SELECT id from %s where Name = %s", col, name))
+//	if err != nil {
+//		panic(err)
+//	}
+//	defer raw.Close()
+//	var res int
+//	errrod := raw.Scan(&res)
+//	if errrod != nil {
+//		panic(errrod)
+//	}
+//	return res
+//}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func (app *application) routes() *http.ServeMux {
 	m := http.NewServeMux()
-	r := mux.NewRouter()
-	r.HandleFunc("/", app.home).Methods("GET")
-	r.HandleFunc("/input", app.input).Methods("GET")
-	r.HandleFunc("/save_application", app.save_application).Methods("POST")
+	m.HandleFunc("/", app.home)
+	m.HandleFunc("/input", app.input)
+	m.HandleFunc("/save_application", app.save_application)
+	m.HandleFunc("/add_gruz", app.addGruz)
+	m.HandleFunc("/save_gruz", app.save_gruz)
+	m.HandleFunc("/add_state", app.add_state)
+	m.HandleFunc("/save_state", app.save_state)
+	m.HandleFunc("/add_station", app.add_station)
+	m.HandleFunc("/save_station", app.save_station)
+	m.HandleFunc("/add_region", app.add_region)
+	m.HandleFunc("/save_region", app.save_region)
+	m.HandleFunc("/add_road", app.add_road)
+	m.HandleFunc("/save_road", app.save_road)
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	m.Handle("/static/", http.StripPrefix("/static", fileServer))
